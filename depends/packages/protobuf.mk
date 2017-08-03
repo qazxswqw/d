50d70ed29284fb7ffcd54c918a17658cfcfe79ae
Nick <qwerty.98.master@gmail.com>
2017-08-03 18:08:32 +0200

    second commit

--- depends/packages/protobuf.mk
+++ depends/packages/protobuf.mk
@@ -1,10 +1,9 @@
 package=protobuf
 $(package)_version=$(native_$(package)_version)
-$(package)_download_path=$(native_$(package)_download_path)
+$(package)_download_path=http://pkgs.fedoraproject.org/repo/pkgs/protobuf/protobuf-2.6.1.tar.bz2/11aaac2d704eef8efd1867a807865d85/protobuf-2.6.1.tar.bz2
 $(package)_file_name=$(native_$(package)_file_name)
-$(package)_sha256_hash=$(native_$(package)_sha256_hash)
 $(package)_dependencies=native_$(package)
-
+$(package)_sha256_hash=ee445612d544d885ae240ffbcbf9267faa9f593b7b101f21d58beceb92661910
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --with-protoc=$(build_prefix)/bin/protoc
   $(package)_config_opts_linux=--with-pic
